95e62320861fd9e92bd75564e992ead818364bcf
Narlugo <etu2017@PBIICO41212.ADLYON2.UNIV-LYON2.FR>
2017-11-21 17:27:29 +0100

    update

--- Assets/Scripts/Orientation.cs
+++ Assets/Scripts/Orientation.cs
@@ -7,4 +7,41 @@ public enum Orientation
     SOUTH,
     EAST,
     WEST
-};
+}
+
+public static class OrientationExt
+{
+    public static Orientation GetToLeft(this Orientation ori)
+    {
+        switch (ori)
+        {
+            case Orientation.NORTH:
+                return Orientation.WEST;
+            case Orientation.SOUTH:
+                return Orientation.EAST;
+            case Orientation.EAST:
+                return Orientation.NORTH;
+            case Orientation.WEST:
+                return Orientation.SOUTH;
+            default:
+                return Orientation.NORTH;
+        }
+    }
+
+    public static Orientation GetToRight(this Orientation ori)
+    {
+        switch (ori)
+        {
+            case Orientation.NORTH:
+                return Orientation.EAST;
+            case Orientation.SOUTH:
+                return Orientation.WEST;
+            case Orientation.EAST:
+                return Orientation.SOUTH;
+            case Orientation.WEST:
+                return Orientation.NORTH;
+            default:
+                return Orientation.NORTH;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     public float speed = 1f;
     public Node curNode;
-    public Orientation startDir;
+    public Orientation curDir;
     public float minDistanceToTurn = .5f;
     private Link curLink;
     private Node nextNode;
@@ -14,33 +14,45 @@ public class Player : MonoBehaviour
 
     private bool stop = false;
 
+    private bool haveTurn = false;
+
     // Use this for initialization
     void Start()
     {
         transform.position = curNode.getPos();
-        curLink = curNode.GetLink(startDir);
+        curLink = curNode.GetLink(curDir);
         nextNode = curLink.GetOpositeNode(curNode);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Left"))
+
+        if (distance > curLink.Distance)
+        {
+            curNode = nextNode;
+            curLink = curNode.GetLink(curDir);
+            nextNode = curLink.GetOpositeNode(curNode);
+            distance = 0;
+        }
+        else if (Input.GetButtonDown("Left"))
         {
             if (curLink.Distance - distance < minDistanceToTurn)
             {
                 curNode = nextNode;
-                curLink = curNode.GetLink(Orientation.WEST);
+                curDir = curDir.GetToLeft();
+                curLink = curNode.GetLink(curDir);
                 nextNode = curLink.GetOpositeNode(curNode);
                 distance = 0;
             }
         }
-        if (Input.GetButtonDown("Right"))
+        else if (Input.GetButtonDown("Right"))
         {
             if (curLink.Distance - distance < minDistanceToTurn)
             {
                 curNode = nextNode;
-                curLink = curNode.GetLink(Orientation.EAST);
+                curDir = curDir.GetToRight();
+                curLink = curNode.GetLink(curDir);
                 nextNode = curLink.GetOpositeNode(curNode);
                 distance = 0;
             }
